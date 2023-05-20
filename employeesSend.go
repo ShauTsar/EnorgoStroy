b4c4fa99a6c7f4350f7a981ea10ccadd567971f0
Shau Tsar <shautsar@mail.ru>
2023-05-20 15:06:16 +0500

    Initial commit

---
+++ employeesSend.go
@@ -0,0 +1,251 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+const (
+	dbURL = "postgresql://postgres:NNA2s*123@localhost:5432/requests?sslmode=disable"
+)
+
+func main() {
+	router := gin.Default()
+
+	// Инициализация хранилища сессий
+	store := cookie.NewStore([]byte("secret"))
+	router.Use(sessions.Sessions("mysession", store))
+
+	router.LoadHTMLGlob("templates/*")
+	router.Static("/static", "./static")
+
+	router.GET("/", showHomePage)
+	router.GET("/add-request", showAddRequestPage)
+	router.POST("/add-request", addRequest)
+	router.GET("/view-requests", viewRequests)
+	router.POST("/mark-as-completed", markRequestAsCompleted)
+	router.GET("/tasks", showTasksPage)
+	router.GET("/send-question", showQuestionPage)
+
+	// Путь для входа
+	router.GET("/login", showLoginPage)
+	router.POST("/login", handleLogin)
+
+	// Middleware для проверки авторизации
+	router.Use(checkAuthMiddleware())
+
+	router.Run(":8088")
+}
+func showQuestionPage(c *gin.Context) {
+	c.HTML(http.StatusOK, "create_question.html", nil)
+}
+
+// Middleware для проверки авторизации
+func checkAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		session := sessions.Default(c)
+
+		// Проверяем, авторизован ли пользователь
+		if auth := session.Get("authenticated"); auth != nil && auth.(bool) {
+			c.Next()
+			return
+		}
+
+		// Если не авторизован, перенаправляем на страницу входа
+		c.Redirect(http.StatusSeeOther, "/login")
+		c.Abort()
+	}
+}
+
+func showLoginPage(c *gin.Context) {
+	c.HTML(http.StatusOK, "login.html", nil)
+}
+
+func handleLogin(c *gin.Context) {
+	session := sessions.Default(c)
+	username := c.PostForm("username")
+	password := c.PostForm("password")
+
+	// Проверка логина и пароля
+	if username == "kadr" && password == "P@Skadr" {
+		// Авторизация прошла успешно, устанавливаем флаг авторизации в сессии
+		session.Set("authenticated", true)
+		session.Save()
+		c.Redirect(http.StatusSeeOther, "/view-requests")
+
+	} else {
+		// Неверные логин или пароль
+		c.HTML(http.StatusOK, "login.html", gin.H{"Error": "Неверный логин или пароль. Попробуйте еще раз."})
+	}
+}
+
+func viewRequests(c *gin.Context) {
+	session := sessions.Default(c)
+
+	// Проверяем, авторизован ли пользователь
+	if auth := session.Get("authenticated"); auth != nil && auth.(bool) {
+		category := c.Query("category")
+
+		// Получите данные заявок из базы данных или другого источника в соответствии с выбранной категорией
+		// В данном примере создан простой список заявок
+		requests, _ := getRequestsFromDB(category)
+		c.HTML(http.StatusOK, "view_requests.html", gin.H{
+			"Authenticated": true,
+			"Requests":      requests,
+		})
+		return
+	}
+	c.HTML(http.StatusOK, "view_requests.html", gin.H{
+		"Authenticated": false,
+	})
+	return
+}
+
+func showHomePage(c *gin.Context) {
+	session := sessions.Default(c)
+	flashMessages := session.Flashes()
+	session.Save()
+
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"FlashMessages": flashMessages,
+	})
+}
+
+func showTasksPage(c *gin.Context) {
+	c.HTML(http.StatusOK, "tasks.html", nil)
+}
+
+func showAddRequestPage(c *gin.Context) {
+	c.HTML(http.StatusOK, "add_request.html", nil)
+}
+
+func addRequest(c *gin.Context) {
+	object := c.PostForm("object")
+	department := c.PostForm("department")
+	brigade := c.PostForm("brigade")
+	employees := c.PostForm("employees")
+
+	// Подключение к базе данных
+	db, err := sql.Open("postgres", "postgresql://postgres:NNA2s*123@localhost:5432/requests?sslmode=disable")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to connect to database"})
+		fmt.Printf("error: %v", err)
+		return
+	}
+	defer db.Close()
+
+	completed := false
+	// Вставка данных в таблицу requests
+	insertQuery := "INSERT INTO requests (object, department, brigade, employees, completed) VALUES ($1, $2, $3, $4, $5)"
+	_, err = db.Exec(insertQuery, object, department, brigade, employees, completed)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to add request"})
+		return
+	}
+
+	// Добавление флэш-сообщения о успешной отправке
+	session := sessions.Default(c)
+	session.AddFlash("Отправка успешно проведена")
+	session.Save()
+
+	c.Redirect(http.StatusSeeOther, "/")
+}
+
+func getRequestsFromDB(category string) ([]map[string]interface{}, error) {
+	db, err := sql.Open("postgres", "postgresql://postgres:NNA2s*123@localhost:5432/requests?sslmode=disable")
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database: %v", err)
+	}
+	defer db.Close()
+	// Построение SQL-запроса в зависимости от выбранной категории
+	var query string
+	var queryParams []interface{}
+	if category == "all" {
+		query = "SELECT * FROM requests ORDER BY completed ASC"
+	} else {
+		query = "SELECT * FROM requests WHERE object = $1 ORDER BY completed ASC"
+		queryParams = append(queryParams, category)
+	}
+
+	rows, err := db.Query(query, queryParams...)
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute SQL query: %v", err)
+	}
+	defer rows.Close()
+
+	columns, err := rows.Columns()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get column names: %v", err)
+	}
+
+	var requests []map[string]interface{}
+	for rows.Next() {
+		// Создание слайса с указателями на значения для каждого столбца
+		values := make([]interface{}, len(columns))
+		pointers := make([]interface{}, len(columns))
+		for i := range values {
+			pointers[i] = &values[i]
+		}
+		// Сканирование строки результата запроса в указатели значений
+		if err := rows.Scan(pointers...); err != nil {
+			return nil, fmt.Errorf("failed to scan row: %v", err)
+		}
+
+		// Создание карты значений для каждой заявки
+		request := make(map[string]interface{})
+		for i, column := range columns {
+			if column == "employees" {
+				// Разделение имен сотрудников по запятой
+				employees := strings.Split(values[i].(string), ",")
+				request[column] = employees
+			} else if column == "completed" {
+				request[column] = values[i]
+			} else {
+				request[column] = values[i]
+			}
+		}
+		requests = append(requests, request)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("error occurred while iterating over rows: %v", err)
+	}
+
+	return requests, nil
+}
+
+var data = ""
+
+func markRequestAsCompleted(c *gin.Context) {
+	// Get the request ID from the form
+	requestID, _ := strconv.Atoi(c.PostForm("requestID"))
+
+	// Update the request status in the database to completed
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	stmt, err := db.Prepare("UPDATE requests SET completed = $1 WHERE id = $2")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(true, requestID)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Set the response status as completed
+	c.Redirect(http.StatusSeeOther, "/view-requests")
+}
---
+++ static/scripts/script.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Проверка, авторизован ли пользователь
+    let authenticated = "{{ .Authenticated }}";
+    if (authenticated === "true") {
+        // Показать выбор категории
+        document.getElementById("login-form").style.display = "none";
+        document.getElementById("request-list").style.display = "block";
+    }
+});
+
+$(document).ready(function() {
+    // Обработчик события изменения значения поля "Объект"
+    $('#object').change(function() {
+        // Получаем выбранное значение объекта
+        let selectedObject = $(this).val();
+
+        // Вызываем функцию для обновления списка подразделений
+        updateDepartments(selectedObject);
+    });
+
+    // Функция для обновления списка подразделений
+    function updateDepartments(selectedObject) {
+        // Определяем список подразделений для каждого объекта
+        let departments = {
+            'Норильск': ['НМЗ 010', 'НМЗ ВЗИС', 'НМЗ Норникель  ПНР', 'НМЗ Норникель Вахтовый поселок', 'НМЗ Норникель ГПП-83', 'НМЗ Норникель Косвенный', 'НМЗ НСК'],
+            'Мурманск': ['Мурманск BC1', 'Мурманск GWP5A', 'Мурманск Косвенный'],
+            'ГПЗ': ['ГПЗ Амурский АВР Метрология', 'ГПЗ Амурский ВЗИС', 'ГПЗ Амурский Метрология Ф3', 'ГПЗ Амурский ПНР КИП Ф2', 'ГПЗ Амурский ПНР КИП Ф3', 'ГПЗ Амурский ПНР КИП Ф4',
+                'ГПЗ Амурский ПНР-ПС Ф2', 'ГПЗ Амурский ПНР-ПС Ф3', 'ГПЗ Амурский ПНР-ПС Ф4', 'ГПЗ Амурский Техперевооружение', 'ГПЗ Амурский Фаза 2', 'ГПЗ Амурский Фаза 3', 'ГПЗ Амурский Фаза 4', 'ГПЗ Амурский Фаза 5'
+                , 'ГПЗ Косвенный']
+        };
+        // Очищаем текущий список подразделений
+        $('#department').empty();
+
+        // Получаем список подразделений для выбранного объекта
+        let selectedDepartments = departments[selectedObject];
+
+        // Добавляем опции в список подразделений
+        for (let i = 0; i < selectedDepartments.length; i++) {
+            let department = selectedDepartments[i];
+            $('#department').append('<option value="' + department + '">' + department + '</option>');
+        }
+    }
+});
+
